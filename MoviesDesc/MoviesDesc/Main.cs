8b775abc338d6b68721ce765f9210f0d1f7fe5d9
kuba-murky <kuba.murky@gmail.com>
2013-02-19 12:56:25 +0100

    Kod błędu parsowania parametrów i przykład użycia parsera

--- MoviesDesc/MoviesDesc/Main.cs
+++ MoviesDesc/MoviesDesc/Main.cs
@@ -4,13 +4,27 @@ namespace MoviesDesc
 {
 	class MainClass
 	{
+		public static string Version="0.0.1";
+		
 		public static int Main (string[] args)
 		{
 			//1. Parse program arguments
 			ProgramParams PrgPar=new ProgramParams();
-			if (!PrgPar.ParseArgs(args)) {
-				Console.WriteLine("Cannot parse program parameters. Try run only with: -h");
+			int p;
+			if ((p=PrgPar.ParseArgs(args))!=0) {
+				Console.Error.WriteLine("Cannot parse given parameter number {0}. Try run only with: -h",p);
 				return -1;
+			}
+			
+			if (PrgPar.ShowHelp) {
+				Console.WriteLine("MoviesDesc");
+				Console.WriteLine(PrgPar.GetHelp());
+				return 0;
+			}
+			
+			if (PrgPar.ShowVersion) {
+				Console.WriteLine("MoviesDesc v.{0}",Version);
+				return 0;
 			}
 			
 			return 0;
